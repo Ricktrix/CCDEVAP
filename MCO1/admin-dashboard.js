1fadceb7f189cd05693dd7736b29333cced9b80a
Ricktrix <john_erick_miciano@dlsu.edu.ph>
2026-06-12 10:36:00 +0800

    Add files via upload

---
+++ MCO1/admin-dashboard.js
@@ -0,0 +1,17 @@
+/**
+ *  Admin-dashboard.js
+ *  Lorem Ipsum Airlines Online Ticketing System
+ *  Admin Dashboard Functionality
+ */
+
+$(document).ready(function () {
+    /**
+     *  Global Variables and Configuration 
+     */
+
+    // Dashbaord Statistics Configuration
+    const dasboardConfig = {
+        totalBookingValue: 1542,
+        totalRevenueValue: 582340,
+    }
+})
